70ad40cff5f9514ab19aa4f358b45a70e0efad51
Pedro Guilherme <pedroguinasc2002@gmail.com>
2023-09-08 21:46:19 -0300

    get it

--- src/pages/AdminRoom/AdminRoom.tsx
+++ src/pages/AdminRoom/AdminRoom.tsx
@@ -1,4 +1,3 @@
-import {useState} from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import logoImg from '../../assets/images/logo.svg'
 import checkImg from '../../assets/images/check.svg'
